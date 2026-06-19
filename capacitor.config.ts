1da9c059d3a85ea6e2747d698261ef38e66e0c51
Doscharl <dosso.choilio@gmail.com>
2026-06-19 18:00:17 +0000

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dosco.game',
+  appName: 'DOSCO',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
+``
